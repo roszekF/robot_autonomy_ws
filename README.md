4b57ffb9ad4fd3e1ac471d3d8d1dbb91fd261080
Filip Roszkowski <roszkowskifilip@gmail.com>
2024-04-19 11:31:38 +0200

    map topic

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # robot_autonomy_ws
+
+
+```shell
+source install/setup.bash
+ros2 launch my_turtlebot turtlebot_simulation.launch.py
+ros2 run final_project map_publisher
+```
--- src/final_project/final_project/map_publisher.py
+++ src/final_project/final_project/map_publisher.py
@@ -1,37 +1,51 @@
 import rclpy
+import numpy as np
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from nav_msgs.msg import OccupancyGrid
 
 
-class MinimalPublisher(Node):
+class MapPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        self.publisher_ = self.create_publisher(OccupancyGrid, '/custom_map', 10)
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        self.grid = np.array([0] * 20000, dtype=np.int8)
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = OccupancyGrid()
+
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'map'
+        msg.info.map_load_time = self.get_clock().now().to_msg()
+        msg.info.resolution = 0.1 # meters per cell
+        msg.info.width = 200
+        msg.info.height = 100
+        msg.info.origin.position.x = -10.0
+        msg.info.origin.position.y = -5.0
+        msg.info.origin.position.z = 0.0
+        msg.info.origin.orientation.x = 0.0
+        msg.info.origin.orientation.y = 0.0
+        msg.info.origin.orientation.z = 0.0
+        msg.info.origin.orientation.w = 1.0
+        msg.data = self.grid.tolist()
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing map')
         self.i += 1
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    map_publisher = MapPublisher()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(map_publisher)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    map_publisher.destroy_node()
     rclpy.shutdown()
 
 
