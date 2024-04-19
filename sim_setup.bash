1b9178bf3d471abb537e14dc5f9ffb4fca61dd2f
Filip Roszkowski <roszkowskifilip@gmail.com>
2024-04-19 13:11:14 +0200

    fix

--- sim_setup.bash
+++ sim_setup.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export ROS_DOMAIN_ID=11
 export TURTLEBOT3_MODEL=burger
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix my_turtlebot)/share/my_turtlebot/models
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix turtlebot3_gazebo)/share/turtlebot3_gazebo/models
