bc5150511e70ef8630f3ea267bf59c73b386e36a
s233461 <s233461@dtu.dk>
2024-04-23 00:15:01 +0200

    Improving comments

--- src/final_project/final_project/bt_publisher.py
+++ src/final_project/final_project/bt_publisher.py
@@ -12,14 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# std publisher dependencies
 import rclpy
-import time
-
 from rclpy.node import Node
-
-from std_msgs.msg import String
+# External dependencies 
 from geometry_msgs.msg import Twist
-
+import time
 
 class BtRoot():
     ''' Params: Array of childs '''
@@ -61,7 +59,8 @@ class BtRepeatSequential():
         return True     
 
 class BtActionNode():
-    ''' params: function, args ''' 
+    ''' Action nodes links functions to callable nodes 
+        params: function, args ''' 
     def __init__(self,child, args):
         self.child = child
         self.args = args
@@ -89,12 +88,11 @@ def stop_fn(pub):
     pub.publisher_.publish(msg) 
 
 def spin_fn(pub):
-    print("spin called")
+    print("spin called") # Debug 
     msg = Twist()
     # Set linear velocity (m/s) for moving straight
-    msg.angular.z = 50.7  # No angular velocity for straight movement
-    msg.linear.x  = 0.1  # adjust this value as needed
-
+    msg.angular.z = 50.7  # high angular velocity for avoiding getting stuck  
+    msg.linear.x  = 0.1 
     pub.publisher_.publish(msg)
     print("message published")
     time.sleep(1.0) # 1.45
@@ -102,9 +100,9 @@ def spin_fn(pub):
         
 
 def move_ahead_incremental(pub, sleep_time):
-    '''Move at 0.2 in linear X during a sleep time
+    '''Move at 0.5 in linear X during a sleep time
        param: publisher, sleep_time'''
-    print("From move fn",sleep_time)
+    print("From move fn",sleep_time) # Debug 
     msg = Twist()
     # Set linear velocity (m/s) for moving straight
     msg.angular.z = 0.0  # No angular velocity for straight movement
@@ -121,21 +119,21 @@ def increment_fn(x, increment):
 
 def main(args=None):
     rclpy.init(args=args)
-    
     minimal_publisher = MinimalPublisher()
+    # Sleep time is used as an incremental counter to move ahead. IE of sleep time values: 2.0, 2.2, 2.4 ...
+    # Whit this approach we achive the spiral 
     sleep_time = [2.0]  # function input. 
-
+    #Setting up the BT
     spin = BtActionNode(spin_fn, [minimal_publisher])
     move =  BtActionNode(move_ahead_incremental, [minimal_publisher,  sleep_time])
     increment = BtActionNode(increment_fn, [sleep_time, 0.30])
     sq1 = BtSequential([move, spin, increment])
     repeat = BtRepeatSequential(sq1, 100)
     root = BtRoot([repeat])
-    root.run()
-
-    print("Returned from root")
+    # Execute the BT
+    root.run() 
+    print("Returned from root") # We know the node is done # Debug propourses
     rclpy.spin(minimal_publisher)
-    
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
