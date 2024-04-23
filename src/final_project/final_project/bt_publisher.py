b63ab1b9e3fcd44edaf7d09221ece1dda1973cda
s233461 <s233461@dtu.dk>
2024-04-23 20:59:37 +0200

    pre rumba.

--- src/final_project/final_project/bt_publisher.py
+++ src/final_project/final_project/bt_publisher.py
@@ -15,7 +15,11 @@
 # std publisher dependencies
 import rclpy
 from rclpy.node import Node
-# External dependencies 
+# External dependencies
+from nav_msgs.msg import OccupancyGrid 
+
+from sensor_msgs.msg import LaserScan
+
 from geometry_msgs.msg import Twist
 import time
 
@@ -66,14 +70,23 @@ class BtActionNode():
         self.args = args
 
     def run(self):
-        try:
-            ret = self.child(*self.args)
-        except:  
-            ret = False 
-        return ret        
+        ret = self.child(*self.args)
+        return ret      
+      
+class LaserSubscriber(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(LaserScan, '/scan', self.listener_callback, 11)
+        print('sub created')
+        self.subscription  # prevent unused variable warning
+        self.msg
 
+    def listener_callback(self, msg):
+        self.msg = msg.ranges
+        #print("listener callback called")
+        #self.get_logger().info('I heard: "%s"' % msg.ranges)    
 
-class MinimalPublisher(Node):
+class TwistPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
@@ -81,7 +94,8 @@ class MinimalPublisher(Node):
         timer_period = 0.5  # seconds
         self.i = 0
 
-def stop_fn(pub):
+
+def stop_movement_fn(pub):
     msg = Twist()
     msg.angular.z = 0.0
     msg.linear.x = 0.0
@@ -91,12 +105,12 @@ def spin_fn(pub):
     print("spin called") # Debug 
     msg = Twist()
     # Set linear velocity (m/s) for moving straight
-    msg.angular.z = 0.7  # high angular velocity for avoiding getting stuck  
+    msg.angular.z = 50.7  # high angular velocity for avoiding getting stuck  
     msg.linear.x  = 0.1 
     pub.publisher_.publish(msg)
     print("message published")
     time.sleep(1.0) # 1.45
-    stop_fn(pub)
+    stop_movement_fn(pub)
         
 
 def move_ahead_incremental(pub, sleep_time):
@@ -110,34 +124,42 @@ def move_ahead_incremental(pub, sleep_time):
     pub.publisher_.publish(msg)
     print("message published")
     time.sleep(sleep_time[0])
-    stop_fn(pub)
+    stop_movement_fn(pub)
 
 
 def increment_fn(x, increment):
     x[0] += increment # Modify the value inside the list
     print(x[0])
 
+def not_crashing_fn(lasser_info, increment):
+    pass
+    # return true or false 
+
 def main(args=None):
+    print("sym")
     rclpy.init(args=args)
-    minimal_publisher = MinimalPublisher()
+    lidar_subscriber = LaserSubscriber()
+    rclpy.spin(lidar_subscriber)
+    twist_publisher = TwistPublisher()
     # Sleep time is used as an incremental counter to move ahead. IE of sleep time values: 2.0, 2.2, 2.4 ...
     # Whit this approach we achive the spiral 
     sleep_time = [2.0]  # function input. 
     #Setting up the BT
-    spin = BtActionNode(spin_fn, [minimal_publisher])
-    move =  BtActionNode(move_ahead_incremental, [minimal_publisher,  sleep_time])
+    spin = BtActionNode(spin_fn, [twist_publisher])
+    move =  BtActionNode(move_ahead_incremental, [twist_publisher,  sleep_time])
     increment = BtActionNode(increment_fn, [sleep_time, 0.30])
+    not_crashing =  BtActionNode(not_crashing_fn, [lidar_subscriber.msg]) # Does the value get frozzen? Chek increment fn if it does
     sq1 = BtSequential([move, spin, increment])
     repeat = BtRepeatSequential(sq1, 100)
     root = BtRoot([repeat])
     # Execute the BT
     root.run() 
     print("Returned from root") # We know the node is done # Debug propourses
-    rclpy.spin(minimal_publisher)
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    lidar_subscriber.destroy_node()
+    twist_publisher.destroy_node()
     rclpy.shutdown()
 
 
